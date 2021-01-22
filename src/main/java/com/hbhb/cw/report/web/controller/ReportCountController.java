79d6611d51b0583be4da21a5f77950797ae67445
yzc <1336507760@qq.com>
2021-01-22 14:06:53 +0800

    1.22 报表信息导出不上传

--- src/main/java/com/hbhb/cw/report/web/controller/ReportCountController.java
+++ src/main/java/com/hbhb/cw/report/web/controller/ReportCountController.java
@@ -1,24 +1,15 @@
 package com.hbhb.cw.report.web.controller;
 
-import com.hbhb.core.utils.ExcelUtil;
 import com.hbhb.cw.report.service.ReportService;
-import com.hbhb.cw.report.web.vo.ReportCountHallExportVO;
 import com.hbhb.cw.report.web.vo.ReportCountResVO;
-import com.hbhb.cw.report.web.vo.ReportCountUnitExportVO;
 import com.hbhb.cw.report.web.vo.ReportReqVO;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -47,19 +38,19 @@ public class ReportCountController {
         return reportService.getReportCountList(reportReqVO, pageNum, pageSize);
     }
 
-    @Operation(summary = "导出")
-    @PostMapping("/export")
-    public void exportBusiness(HttpServletRequest request, HttpServletResponse response,
-                               @Parameter(description = "条件") @RequestBody ReportReqVO reportReqVO) {
-        ;
-        if (reportReqVO.getHallId() == null) {
-            List<ReportCountUnitExportVO> list = reportService.getReportCountUnitExcel(reportReqVO);
-            String fileName = ExcelUtil.encodingFileName(request, "报表上传统计");
-            ExcelUtil.export2Web(response, fileName, fileName, ReportCountUnitExportVO.class, list);
-        } else {
-            List<ReportCountHallExportVO> list = reportService.getReportCountHallExcel(reportReqVO);
-            String fileName = ExcelUtil.encodingFileName(request, "报表上传统计");
-            ExcelUtil.export2Web(response, fileName, fileName, ReportCountHallExportVO.class, list);
-        }
-    }
+//    @Operation(summary = "导出")
+//    @PostMapping("/export")
+//    public void exportBusiness(HttpServletRequest request, HttpServletResponse response,
+//                               @Parameter(description = "条件") @RequestBody ReportReqVO reportReqVO) {
+//        ;
+//        if (reportReqVO.getHallId() == null) {
+//            List<ReportCountUnitExportVO> list = reportService.getReportCountUnitExcel(reportReqVO);
+//            String fileName = ExcelUtil.encodingFileName(request, "报表上传统计");
+//            ExcelUtil.export2Web(response, fileName, fileName, ReportCountUnitExportVO.class, list);
+//        } else {
+//            List<ReportCountHallExportVO> list = reportService.getReportCountHallExcel(reportReqVO);
+//            String fileName = ExcelUtil.encodingFileName(request, "报表上传统计");
+//            ExcelUtil.export2Web(response, fileName, fileName, ReportCountHallExportVO.class, list);
+//        }
+//    }
 }
